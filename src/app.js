7d837e7a63d3ab9cb8a3cb795093f2ff3e39409a
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-08-13 10:10:17 +0530

    added user routes to app.js...

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
+//import routers
+import { userRouter } from "./routes/user.routes.js";
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 export default app;
